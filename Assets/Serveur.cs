380debabf174e7c24060067b34e8d9f0385c4ccb
Corentin Jouan <corentin.jouan@bdtechservices.fr>
2026-04-29 02:22:04 +0200

    ex 7

--- Assets/Serveur.cs
+++ Assets/Serveur.cs
@@ -1,65 +1,78 @@
 using System;
+using System.Text;
 using System.Net;
+using UnityEngine;
 using System.Net.Sockets;
-using System.Threading;
+using System.Threading.Tasks;
 
 class Serveur {
-    public static Thread zt;
+    private static TcpListener listener;
+    private static bool actif = false;
     private static int portServeur;
-    private static readonly System.Object l = new System.Object();
+    private static readonly object l = new object();
     public static int connexionsTraitees = 0;
     public static InfoList logs = new InfoList(10);
-    private static FileAttente missions = new FileAttente();
-    private const int NB_THREADS_POOL = 5;
 
     public static int nouveauTraitement() {
-        lock(l) {
+        lock (l) {
             return connexionsTraitees++;
         }
     }
 
-    private static void WorkerLoop() {
-        while (true) {
-            Socket s = missions.Defiler();
-            ProtocoleServeur p = new ProtocoleServeur(s);
-            p.Protocole();
+    public static async void Demarre(int port) {
+        if (actif) return;
+        portServeur = port;
+        actif = true;
+
+        try {
+            listener = new TcpListener(IPAddress.Any, portServeur);
+            listener.Start(Client.NB_CL);
+
+            while (actif) {
+                Socket ear = await listener.AcceptSocketAsync();
+                _ = ConnexionAsync(ear);
+            }
+        }
+        catch (Exception e) {
+            Debug.Log("Serveur arrêté : " + e.Message);
+            actif = false;
         }
     }
 
-    public static void Run() {
-        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private static async Task ConnexionAsync(Socket ear) {
         try {
-            for (int i = 0; i < NB_THREADS_POOL; i++) {
-                Thread t = new Thread(new ThreadStart(WorkerLoop));
-                t.IsBackground = true;
-                t.Start();
-            }
+            byte[] buffer = new byte[2048];
+            int lus = await Task.Factory.FromAsync(
+                ear.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, null, null),
+                ear.EndReceive);
 
-            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            sock.Bind(new IPEndPoint(IPAddress.Any, portServeur));
-            sock.Listen(Client.NB_CL);
+            if (lus > 0) {
+                string msg = Encoding.ASCII.GetString(buffer, 0, lus);
+                int ml = msg.Length;
 
-            while (true) {
-                Socket ear = sock.Accept();
-                if (ear != null) {
-                    missions.Enfiler(ear);
+                if (msg.StartsWith("POST") && msg[ml - 1] == '\n') {
+                    string data = msg.Substring(5, ml - 6);
+                    logs.addItem("From: " + ear.RemoteEndPoint + " : " + data);
+                    
+                    string reponse = "" + nouveauTraitement();
+                    byte[] respData = Encoding.ASCII.GetBytes(reponse);
+                    
+                    await Task.Factory.FromAsync(
+                        ear.BeginSend(respData, 0, respData.Length, SocketFlags.None, null, null),
+                        ear.EndSend);
                 }
             }
         }
         catch (Exception e) {
-            if (sock != null) sock.Close();
-            zt = null;
-            throw e;
+            Debug.Log("Erreur connexion : " + e.Message);
         }
         finally {
-            zt = null;
+            ear.Close();
         }
     }
 
-    public static void Demarre(int port) {
-        if (zt != null) return;
-        portServeur = port;
-        zt = new Thread(new ThreadStart(Run));
-        zt.Start();
+    public static void Stop() {
+        actif = false;
+        listener?.Stop();
     }
 }
